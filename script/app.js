3207d2b7e96b253036c0cc64d811a284f1b4d265
anastasijaz <92513530+anastasijaz@users.noreply.github.com>
2022-01-22 18:49:48 +0100

    add displaytemperatiure

--- script/app.js
+++ script/app.js
@@ -76,13 +76,38 @@ function displayForecast(response) {
   console.log(forecastHTML);
 }
 
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+
+  celsiusTemperature = response.data.main.temp;
+
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
+}
 function searchCity(event) {
   event.preventDefault();
   let city = document.querySelector("#city-input").value;
   let apiKey = "ad6adba1de9c56cc7cb494546cf33bc9";
   let units = "metric";
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
-  axios.get(url).then(displayForecast);
+  axios.get(url).then(displayTemperature);
 }
 // search Location
 function searchLocation(position) {
