f37d1f09588c87dd34d6eccc40c5dc0a6b66ab49
anastasijaz <92513530+anastasijaz@users.noreply.github.com>
2022-01-22 18:52:26 +0100

    add getForecast

--- script/app.js
+++ script/app.js
@@ -75,6 +75,12 @@ function displayForecast(response) {
   forecastElement.innerHTML = forecastHTML;
   console.log(forecastHTML);
 }
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
 
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
@@ -101,6 +107,7 @@ function displayTemperature(response) {
 
   getForecast(response.data.coord);
 }
+
 function searchCity(event) {
   event.preventDefault();
   let city = document.querySelector("#city-input").value;
