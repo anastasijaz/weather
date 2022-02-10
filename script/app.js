d252a3ba2b9742485f1291f95d39e8fce350689c
anastasijaz <92513530+anastasijaz@users.noreply.github.com>
2022-02-10 19:11:01 +0100

    delete displayForecast

--- script/app.js
+++ script/app.js
@@ -38,72 +38,11 @@ function displayDate(timestamp) {
   return day;
 }
 
-function displayForecast(response) {
-  let forecastData = response.data.daily;
-  let forecastElement = document.querySelector("#forecast");
-
-  let forecastHTML = `<div class="row">`;
-  forecastData.forEach(function (forecastDay, index) {
-    if (index < 6) {
-      forecastHTML =
-        forecastHTML +
-        `
-      <div class="col-2">
-        <div class="weather-forecast-date">${displayDate(forecastDay.dt)}</div>
-        <img
-          src="http://openweathermap.org/img/wn/${
-            forecastday.weather[0].icon
-          }@4x.png"
-          alt=""
-          width=""
-          class="img-forecast"
-        />
-        <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-temperature-max"> ${Math.round(
-            forecastDay.temp.max
-          )}°</span>
-          <span class="weather-forecast-temperature-min"> ${Math.round(
-            forecastDay.temp.min
-          )}°</span>
-        </div>
-      </div>
-  `;
-    }
-  });
-
-  forecastHTML = forecastHTML + `</div>`;
-  forecastElement.innerHTML = forecastHTML;
-}
 function getWeeklyForecast(coordinates) {
   console.log(coordinates);
   let apiKey = "ad6adba1de9c56cc7cb494546cf33bc9";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
-}
-
-function displayTemperature(response) {
-  let temperatureElement = document.querySelector("#temperature");
-  let cityElement = document.querySelector("#city-input");
-  let descriptionElement = document.querySelector("#description");
-  let humidityElement = document.querySelector("#humidity");
-  let windElement = document.querySelector("#wind");
-  let dateElement = document.querySelector("#date");
-  let iconElement = document.querySelector("#icon");
-
-  celsiusTemperature.innerHTML = response.data.main.temp;
-  temperatureElement.innerHTML = Math.round(celsiusTemperature);
-  cityElement.innerHTML = response.data.name;
-  descriptionElement.innerHTML = response.data.weather[0].description;
-  humidityElement.innerHTML = response.data.main.humidity;
-  windElement.innerHTML = Math.round(response.data.wind.speed);
-  dateElement.innerHTML = formatDate(response.data.dt * 1000);
-  iconElement.setAttribute(
-    "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-  );
-  iconElement.setAttribute("alt", response.data.weather[0].description);
-
-  getForecast(response.data.coord);
 }
 
 function searchCity(event) {
@@ -130,42 +69,10 @@ function getCurrentPositon(event) {
   navigator.geolocation.getCurrentPosition(searchLocation);
 }
 
-//Conversions
-function metricToImperial(event) {
-  let TempElement = document.querySelector("#sun");
-  let TempMaxElement = document.querySelector("#maxTemp");
-  let TempMinElement = document.querySelector("#minTemp");
-
-  let FahrenheitTemperatur = (celciusTemp * 9) / 5 + 32;
-  let FahrenheitMaxTemperatur = (celciusMaxTemp * 9) / 5 + 32;
-  let FahrenheitMinTemperatur = (celciusMinTemp * 9) / 5 + 32;
-
-  TempElement.innerHTML = `${Math.round(FahrenheitTemperatur)}°F`;
-  TempMaxElement.innerHTML = `${Math.round(FahrenheitMaxTemperatur)}°F`;
-  TempMinElement.innerHTML = `${Math.round(FahrenheitMinTemperatur)}°F`;
+function displayForecast() {
+  let temp = document.querySelector("#sun");
+  temp.innerHTML = `20°C`;
 }
-
-function ImperialToMetric(event) {
-  document.querySelector("#sun").innerHTML = `${Math.round(celciusTemp)}°C`;
-  document.querySelector("#maxTemp").innerHTML = `${Math.round(
-    celciusMaxTemp
-  )}°C`;
-  document.querySelector("#minTemp").innerHTML = `${Math.round(
-    celciusMinTemp
-  )}°C`;
-}
-
-//Temp Buttons
-let FahrenheitBnt = document.querySelector("#imperial-value");
-FahrenheitBnt.addEventListener("click", metricToImperial);
-
-let CelciusBnt = document.querySelector("#metric-value");
-CelciusBnt.addEventListener("click", ImperialToMetric);
-
-let celciusTemp = null;
-let celciusMaxTemp = null;
-let celciusMinTemp = null;
-
 //Location Buttons
 let cityBttn = document.querySelector("#location");
 cityBttn.addEventListener("click", getCurrentPositon);
